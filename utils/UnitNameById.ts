34a4e77a332d56b363bae9f51a28c674e0c0e5a3
dnipy <dnipy@protonmail.com>
2023-01-31 21:00:16 +0330

    add check-char and unitnamebyid helper funcs

---
+++ utils/UnitNameById.ts
@@ -0,0 +1,40 @@
+export function UnitNameById (id : number) {
+    switch(id){
+        case 1:
+            return "کیلوگرم"
+            
+        case 2:
+            return "کیسه"
+            
+        case 3 : 
+            return "کارتن"
+
+        case 4:
+            return "عدد"
+            
+        case 5:
+            return "حلب"
+            
+        case 6 : 
+            return "پالت"
+    
+        case 7:
+            return "باکس"
+            
+        case 8:
+            return "شل"
+            
+        case 9 : 
+            return "شرینگ"
+    
+        case 10:
+            return "بسته"
+            
+        case 11:
+            return "تن"
+            
+        default: 
+            return "کیلوگرم "
+
+    }
+}
---
+++ utils/chech-chars.ts
@@ -0,0 +1,5 @@
+export function containsSpecialChars(str : string) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
+}
+  
