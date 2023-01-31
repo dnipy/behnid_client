6415876cf8afdb9b2adbdc7e526b04ef3d05427c
dnipy <dnipy@protonmail.com>
2023-01-31 20:54:17 +0330

    add static json files to avoild fetch static db  rows such as categories and cities

---
+++ static/_index.js
@@ -0,0 +1,20 @@
+import Categories from './categories.json' assert {type: 'json'};
+import Cities from "./cities.json" assert {type: 'json'};
+import MainCategories from "./MainCategories.json" assert {type: 'json'};
+import proviences from "./proviences.json" assert {type: 'json'};
+import SubCategories from "./SubCategories.json" assert {type: 'json'};
+import ALlUnits from "./AllUnits.json" assert {type : 'json'}
+import WholeCountry from "./WholeCountry.json" assert {type : 'json'}
+import WholeCategories from "./wholeCategories.json" assert {type : 'json'}
+
+
+export {
+    Categories,
+    Cities,
+    MainCategories,
+    proviences,
+    SubCategories,
+    ALlUnits,
+    WholeCountry,
+    WholeCategories
+}
