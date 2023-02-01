266b3ab3c21f4db64f6aaf26c3a5a3dd727482e1
dnipy <dnipy@protonmail.com>
2023-02-01 16:10:02 +0330

    /features/component added

---
+++ features/components/category_picker.tsx
@@ -0,0 +1,152 @@
+import { useState } from "react"
+import { BiDownArrowAlt } from "react-icons/bi"
+import ErrorComponent from "../../components/alerts/error"
+import { WholeCountry , WholeCategories } from "../../static/_index"
+import { I_add_products } from "../../types/add-products"
+
+
+export const CategoryPickerModel = (props : { fildes : I_add_products , setFileds :React.Dispatch<React.SetStateAction<I_add_products>>  })=>{
+    const [error,setError] = useState('')
+    const { fildes , setFileds } = props
+    const [openedCat,setOpenedCat] = useState(0)
+    const [openedSubCat,setOpenedSubCat] = useState(0)
+
+    return (
+    <>
+      {error ? <ErrorComponent message={error} handle={setError} /> : null}
+      
+        <div className='fixed w-screen h-screen backdrop-blur-sm bg-white/20 z-40 ' >
+    
+            {/* CENTER_DATA_PART */}
+            <div className='fixed flex w-screen h-screen justify-center items-center'>
+                <div dir='rtl' className='w-[380px]  md:w-[600px] min-w-[370px]  mx-auto h-[70vh]  bg-white rounded-3xl overflow-y-auto  '>
+                    <div>
+                        <div className="flex justify-between h-[60px] my-2 gap-5 items-center">
+
+                            <div>
+                                <h1 className="pr-7 text-xl font-bold text-beh-text-gray">
+                                    {/* شهر انتخاب شده */}
+                                    انتخاب دسته 
+                                </h1>
+                            </div>
+
+                            <div>
+                                <h1 onClick={()=>setFileds({...fildes , cat_id: 1 ,showCatPicker : false})} className="pl-7 text-xl font-bold text-beh-orange">
+                                    انصراف
+                                </h1>
+                            </div>
+
+                        {/* </div>
+
+                        <div className="my-2 h-[50px]"> */}
+
+                        </div>
+                        
+                        {/* <div className="my-2 h-[65px] flex justify-center items-center w-full">
+                            <label className="relative block w-10/12">
+                                <input type="text" className='h-[60px] w-full bg-beh-gray-light rounded-sm px-10 placeholder:text-beh-gray-dark placeholder:text-lg ' placeholder='جست و جو در شهر ها' dir='rtl'/>
+                                <span className="absolute inset-y-0 right-3 flex items-center pl-3" >
+                                    <svg className="h-5 w-5 fill-beh-gray-dark" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30"
+                                        height="30" viewBox="0 0 30 30">
+                                        <path
+                                            d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
+                                        </path>
+                                    </svg>
+                                </span>
+                            </label>
+                        </div> */}
+                        <div className="h-[25px] border-b-2 border-beh-gray-light w-full"></div>
+
+
+                        {/* CITY_SELECT_PART */}
+                        <div dir="ltr" className="w-full my-1 overflow-y-auto h-[45vh] border-b-2 border-beh-gray-light">
+                            {WholeCategories.map(elm=>(
+                                <>
+                                    <div key={elm.id} dir="rtl" onClick={()=>{
+                                        if (openedSubCat != elm.id) {
+                                            setOpenedSubCat(elm.id)
+                                        }
+                                        else {
+                                            setOpenedSubCat(0)
+                                        }
+                                        
+                                        
+                                        }} className="w-[90%] hover:text-beh-orange text-beh-gray duration-100 mx-auto rounded-md h-10 my-2 bg-beh-gray-light flex justify-between items-center px-5  gap-5">
+                                        <div>
+                                            <h1 className="font-bold text-lg ">
+                                                {elm.name}
+                                            </h1>
+                                        </div>
+                                        <div className="flex">
+                                            <BiDownArrowAlt className="w-6 h-6" />
+                                        </div>
+                                    </div>
+                                    {openedSubCat == elm.id 
+                                        ? 
+                                            <div dir="rtl" onClick={()=>setOpenedCat(elm.id)} className="w-[70%]  hover:text-beh-orange duration-100 mx-auto rounded-md my-2  px-5  ">
+                                                {
+                                                    elm.subCategories.map(sub_cat=>(
+                                                            <>
+                                                            <div key={sub_cat.id} onClick={()=>setOpenedSubCat(sub_cat.id)} className="w-[90%] hover:text-beh-orange text-beh-gray duration-100 mx-auto rounded-md h-10 my-2 bg-beh-gray-light flex justify-between items-center px-5  gap-5">
+                                                                <div>
+                                                                    <h1 className="text-center">
+                                                                        {sub_cat.name}
+                                                                    </h1>
+                                                                </div>
+
+                                                                <div>
+                                                                    <BiDownArrowAlt className="w-6 h-6" />
+                                                                </div>
+                                                            </div>
+                                                                {
+                                                                    openedCat == sub_cat.id 
+                                                                        ?
+                                                                            sub_cat.categories.map(cat =>(
+                                                                                <div key={cat.id} onClick={()=>setFileds({...fildes,cat_id : cat.id, cat_name : cat.name , showCatPicker : false})} className="w-[80%] hover:text-beh-orange text-beh-gray duration-100 mx-auto rounded-md h-10 my-2 bg-beh-gray-light flex justify-center items-center px-5  gap-5">
+                                                                                    <h1 className="text-center">
+                                                                                        {cat.name}
+                                                                                    </h1>
+                                                                                </div>
+                                                                            ))
+                                                                        :
+                                                                            null
+                                                                }
+                                                            </>
+                                                        ))
+                                                    }
+                                            </div>
+                                        :   
+                                             null
+                                    }
+
+                                </>
+                            ))}
+                        </div>
+
+                        
+
+                        {/* BUTTON_PART */}
+                        {/* <div className="flex justify-around h-[60px] my-2 gap-5 items-center">
+
+                            <div className="w-1/2 py-3 mr-3 rounded-md bg-beh-orange flex justify-center items-center ">
+                                <h1 className="text-xl font-bold text-white">
+                                    انصراف
+                                </h1>
+                            </div>
+                            <div className="w-1/2 py-3 ml-3 rounded-md bg-beh-green-super-light flex justify-center items-center ">
+                                <h1 className="text-xl font-bold text-white">
+                                    تایید
+                                </h1>                            
+                            </div>
+
+                        </div> */}
+                    
+
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
---
+++ features/components/setup-profile-model.tsx
@@ -0,0 +1,168 @@
+import { useRef, useState } from "react";
+import { AuthorizedApiRequest, AuthorizedApiRequestImage } from "../../clients/axios";
+import ErrorComponent from "../../components/alerts/error";
+import SuccesComponent from "../../components/alerts/succes";
+
+export const SetupProfileComponent = (props : { handleClose :  React.Dispatch<React.SetStateAction<boolean | null>> }) => {
+    const [fields,setFields] = useState({name : '' , lastname : '' , pass : '' , pass_confirm : '' })
+    const [error, setError] = useState('');
+    const [succes,setSucces] = useState('')
+    const [loading , setloading] = useState(false);
+    const [selectedImage,setSelectedImage] = useState<File | null>(null)
+    const [policyChecked,setPolicyChecked] = useState(false)
+  
+    const inputFile = useRef<HTMLInputElement | null>(null) 
+  
+    const onButtonClick = () => {
+      inputFile?.current?.click();
+    };
+  
+    const onImageChange= (e: React.ChangeEvent<HTMLInputElement>)=>{
+      if ( e.target.files &&  e.target?.files?.length > 0) {
+        console.log(e.target.files)
+        setSelectedImage(e.target.files[0])
+      }
+    }
+  
+    const sendHandle = async()=>{
+      setError('')
+      setloading(true)
+      if (fields.pass == fields.pass_confirm) {
+        let fData = fields
+        console.log({fData})
+        await AuthorizedApiRequest
+                  .post('/auth/profile-setup',fData)
+                  .then((res) => {
+                      console.log(res)
+                      if(res.data?.msg) {
+                          console.log(res.data.msg)
+                          props.handleClose(true)
+                      }
+                      else {
+                          setError(res.data.err)
+                     }
+                  })
+                  .catch((err) => {
+                      setError(err?.response?.data?.err);
+                  })
+                  .finally(() => {
+                      setloading(false);
+                  })
+  
+        }
+        setloading(false)
+        setError('پسورد و تایید پسورد باید یکسان باشد')
+      }
+  
+  
+    const sendImageHandle = async ()=>{
+      setloading(true)
+      setError('')
+      const body = new FormData()
+      if(selectedImage){
+          body.append('profile_image',selectedImage as Blob)
+          console.log(selectedImage)
+      
+          AuthorizedApiRequestImage 
+          .post('/media/photo/avatar',body)
+          .then((res) => {
+              console.log(res)
+              if (res.data.err) {
+                  setError(res.data.err)
+              }
+              else {
+                setSucces('تصویر با موفقیت ثبت شد')
+              }
+          })
+          .catch((err) => {
+              console.log(err)
+          })
+          .finally(() => {
+              setloading(false);
+          });
+  
+  
+        }else {
+            setError('فایل انتخاب کنید')
+        }
+      }
+    
+  
+    return (
+      <>
+      {error ? <ErrorComponent message={error} handle={setError} /> : null}
+      {succes ? <SuccesComponent message={succes} handle={setSucces} /> : null}
+      
+      <div className='fixed w-screen h-screen backdrop-blur-sm bg-white/20 z-40 ' >
+        <div className="h-[90vh] w-10/12  mx-auto mt-[5vh] flex justify-center items-center">
+          <div className='min-w-[370px] lg:w-[600px] h-full bg-white overflow-y-auto shadow-2xl ' >
+            <div className="bg-beh-gray-dark w-full h-[280px]  pt-10">
+              <div className="flex w-full  h-[220px] justify-center">
+                <div className="max-w-sm  h-full">
+                  <h1  className='text-center text-beh-gray-light font-semibold text-lg'>برای ادامه اطلاعات زیر را تکمیل کنید</h1>
+                  <div className='flex justify-around h-full gap-2 p-1' >
+                    <div onClick={onButtonClick} className="h-[80%] basis-1/6 cursor-pointer bg-beh-orange flex items-center rounded-md">
+                      <div className="rotate-90 " >
+                      <input type='file' onChange={onImageChange} accept="image/png, image/gif, image/jpeg"  id='file' ref={inputFile} style={{display: 'none'}}/>
+                        <h1 className='font-semibold text-white text-lg' >انتخاب</h1>
+                      </div>
+                    </div>
+                    <div  className="h-[80%] basis-5/6 bg-beh-gray-light rounded-md flex justify-center">
+                      <div>
+                        {/* {selectedImage ? <div className='absolute w-[170px] text-center bg-beh-red font-semibold text-white cursor-pointer' onClick={()=>setSelectedImage(null)}>حذف</div> : null} */}
+                        {selectedImage ? <div className={`absolute w-[170px] ${loading ? 'cursor-default' : 'cursor-pointer'} text-center bg-beh-green-light font-semibold text-white `} onClick={ loading ? undefined : sendImageHandle}>{loading ? 'صبر کنید' : 'تایید'}</div> : null}
+                        {selectedImage ?  <img width='170px' height='170px' src={URL.createObjectURL(selectedImage)} /> : null}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='flex justify-center w-full '>
+                <input type='text' placeholder='کد معرف (اختیاری)' className=' placeholder:font-semibold placeholder:text-white border-b-4 border-beh-gray rounded-md bg-beh-orange min-w-[260px] h-[50px] flex items-center text-center'/>
+              </div>
+  
+  
+              <div className='flex flex-row flex-wrap mt-8 justify-around gap-x-2 gap-y-3'>
+                  <div className='mx-auto'>
+                    <input  value={fields.lastname} onChange={(e)=>setFields({...fields,lastname : e.target.value})} type='text' placeholder='نام خانوادگی' className=' placeholder:font-semibold placeholder:text-white border-b-2 border-beh-gray rounded-md bg-beh-orange min-w-[260px] h-[50px] flex items-center text-center'/>                 
+                  </div>
+  
+                  <div className='mx-auto'>
+                    <input  value={fields.name} onChange={(e)=>setFields({...fields,name : e.target.value})} type='text' placeholder='نام' className=' placeholder:font-semibold placeholder:text-white border-b-2 border-beh-gray rounded-md bg-beh-orange min-w-[260px] h-[50px] flex items-center text-center'/>
+                  </div>
+              </div>
+  
+              <div className='flex flex-row flex-wrap mt-3 justify-around gap-x-2 gap-y-3'>
+                  <div className='mx-auto'>
+                    <input  value={fields.pass_confirm} onChange={(e)=>setFields({...fields,pass_confirm : e.target.value})} type='text' placeholder='تکرار رمز' className=' placeholder:font-semibold placeholder:text-white border-b-2 border-beh-gray rounded-md bg-beh-gray min-w-[260px] h-[50px] flex items-center text-center'/>
+                  </div>
+                  
+                  <div className='mx-auto'>
+                    <input  value={fields.pass} onChange={(e)=>setFields({...fields,pass : e.target.value})} type='text' placeholder='رمز عبور' className=' placeholder:font-semibold placeholder:text-white border-b-2 border-beh-gray rounded-md bg-beh-gray min-w-[260px] h-[50px] flex items-center text-center'/>
+                  </div>
+              </div>
+  
+              <div className='flex flex-row flex-wrap w-full mt-5 justify-between gap-x-2 gap-y-3'>
+                  <div className='mx-auto w-[80%] md:w-[94%]'>
+                    <div  onClick={policyChecked && fields.pass.length >= 8  &&  !loading ? sendHandle : undefined} className={`placeholder:font-semibold   border-b-2 border-beh-gray rounded-md ${policyChecked && fields.pass.length >= 8 ? 'cursor-pointer bg-beh-green-light text-white ' : 'text-beh-gray-dark bg-beh-gray-light'}  w-[100%] h-[50px] flex justify-center items-center text-center`}>
+                      <h1>تایید</h1>
+                    </div>
+                  </div>
+              </div>
+  
+              <div className='flex flex-row flex-wrap w-full mt-2 justify-between gap-x-2 gap-y-3'>
+                  <div dir='rtl'  className='mx-auto w-[80%] md:w-[94%]'>
+                    <div className='flex gap-2'>
+                      <input onChange={()=>setPolicyChecked(!policyChecked)} type="checkbox"className='w-5 mt-1 h-5' />
+                      <h1>قوانین را خوانده و با آن موافقم</h1>
+                    </div>
+                  </div>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
