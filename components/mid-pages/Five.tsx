60ecb6e81aeb7401bda655637837c816dc4a954d
dnipy <dnipy@protonmail.com>
2023-02-12 21:29:08 +0330

    500 404 updated

--- components/mid-pages/Five.tsx
+++ components/mid-pages/Five.tsx
@@ -1,15 +1,16 @@
 import { useRouter } from "next/router"
-import FiveSvg from "../../assets/Five.svg"
+import Five from "../../assets/405.png"
 
 export function FiveOO () {
     const router = useRouter()
     return (
-      <div className='flex justify-center items-center h-screen text-center'>
+      <div className='flex justify-center items-center h-screen bg-[#131A1E] text-center'>
       <div className='w-2/5'> 
-          <FiveSvg/>
           <div dir='rtl' >
-            <p className='text-xl font-bold'>ارور در سرور بهنید</p>
-            <p onClick={()=>router.replace('/')} className='text-xl w-1/2 ml-auto mr-auto text-gray pt-2 cursor-pointer hover:bg-orange-400 hover:text-white rounded-sm pb-3 mt-2 transition-all duration-150 '>بازگشت</p>
+          <img src={Five.src} className='mx-auto' />
+            <p className='text-xl font-semibold text-white py-2'>مشکل هنگام ارتباط با سرور</p>
+            <p className='text-xl  text-white py-2'>لطفا از اتصال دستگاه خود به اینترنت اطمینان حاصل فرمایید</p>
+            <p onClick={()=>router.replace('/')} className='text-xl w-1/2 ml-auto mr-auto text-gray pt-2 bg-beh-orange cursor-pointer text-white hover:bg-orange-400 hover:text-white rounded-sm pb-3 mt-2 transition-all duration-150 '>بازگشت</p>
           </div>
       </div>
       </div>
--- components/mid-pages/Four_O_Four.tsx
+++ components/mid-pages/Four_O_Four.tsx
@@ -1,15 +1,15 @@
 import { useRouter } from "next/router"
-import Four from "../../assets/Four.svg"
+import Four from "../../assets/404.png"
 
 export function FourOFour () {
     const router = useRouter()
     return (
-      <div className='flex justify-center items-center h-screen text-center'>
+      <div className='flex justify-center items-center h-screen bg-[#131A1E] text-center'>
       <div className='w-2/5'> 
-          <Four/>
           <div dir='rtl' >
-            <p className='text-xl font-bold'>یافت نشد ...!</p>
-            <p onClick={()=>router.replace('/')} className='text-xl w-1/2 ml-auto mr-auto text-gray pt-2 cursor-pointer hover:bg-orange-400 hover:text-white rounded-sm pb-3 mt-2 transition-all duration-150 '>بازگشت</p>
+          <img src={Four.src} className='mx-auto' />
+            <p className='text-xl font-semibold text-white'>یافت نشد ...!</p>
+            <p onClick={()=>router.replace('/')} className='text-xl w-1/2 ml-auto mr-auto text-gray pt-2 bg-beh-orange cursor-pointer text-white hover:bg-orange-400 hover:text-white rounded-sm pb-3 mt-2 transition-all duration-150 '>بازگشت</p>
           </div>
       </div>
       </div>
