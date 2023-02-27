90d2e02a0903eeec17bf2ab1ced4cecc5f42a8b5
dnipy <dnipy@protonmail.com>
2023-02-27 06:31:56 +0330

    change some config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins : [
+    require("tailwind-scrollbar")
+  ]
+  ,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
