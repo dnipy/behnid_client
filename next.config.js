3fc772d0557072affe438b5f31eb47f0f18d8846
dnipy <dnipy@protonmail.com>
2023-03-03 16:13:09 +0330

    change configs and add moment-jalali

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("next-pwa")({dest : 'public',register : true})
+const withPWA = require("next-pwa")({
+  dest : 'public',
+  register : true,
+  // customWorkerDir: 'serviceworker'
+})
 
 const nextConfig = withPWA({
   images: {
