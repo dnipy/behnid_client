8ba8c40166bf0ce3145c7be3c41b7134cf45f299
dnipy <dnipy@protonmail.com>
2023-04-26 23:50:04 +0430

    dnipy: add spambyid helper func

---
+++ utils/SpamNameById.ts
@@ -0,0 +1,25 @@
+export function SpamNameById (id : number) {
+    switch(id){
+        case 1:
+            return "violence"
+            
+        case 2:
+            return "unethical"
+            
+        case 3 : 
+            return "Phishing"
+
+        case 4:
+            return "inviteLink"
+            
+        case 5:
+            return "Nuisance"
+            
+        case 6 : 
+            return "racism"
+    
+        default: 
+            return "violence"
+
+    }
+}
